060dd5dc84b1255362052d1606e1362935907f6d
amarkeshri28 <amarjeetkeshri2013@gmil.com>
2022-01-19 10:18:37 +0530

    JWT auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../../models/User');
@@ -45,17 +47,30 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        // return jsonwebtoken
-        res.send('user registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+
+                res.json({ token });
+                // console.log(token);
+            }
+        );
+
+
     } catch (error) {
         console.log(error.message);
         res.status(500).send('server error');
     }
 
-
-
-
-    res.send('User route');
 });
 
 module.exports = router;
