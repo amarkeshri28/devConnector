b6df5f680e6a15a1a6fa44a69e6f57be09add5c1
amarkeshri28 <amarjeetkeshri2013@gmil.com>
2023-05-30 17:04:01 +0530

    Added Like and unlike functionality

--- client/src/components/post/Posts.js
+++ client/src/components/post/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
 import Spinner from '../layout/Spinner'
+import PostItem from './PostItem'
 
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -11,10 +12,20 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     }, [getPosts]);
     return loading ? <Spinner /> : (
         <Fragment>
-            test
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>
+                Welcome to Community
+            </p>
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
         </Fragment>
     )
 }
+
 
 Posts.propTypes = {
     getPosts: PropTypes.func.isRequired,
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { getGithubRepos } from '../../actions/profile'
-import Spinner from '../layout/Spinner';
+// import Spinner from '../layout/Spinner';
+
+
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
     useEffect(() => {
         getGithubRepos(username);
@@ -10,25 +12,23 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
     return (
         <div className="profile-github">
             <h2 className="text-primary my-1">Github Repos</h2>
-            {repos === null ? <Spinner /> : (
-                repos.map(repo => (
-                    <div key={repo._id} className="repo my-1 bg-white p-1">
-                        <div>
-                            <h4>
-                                <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>{repo.name}</a>
-                            </h4>
-                            <p>{repo.description}</p>
-                        </div>
-                        <div>
-                            <ul>
-                                <li className="badge badge-primary">Stars: {repo.stargazers_count}</li>
-                                <li className="badge badge-dark">Watchers: {repo.watchers_count}</li>
-                                <li className="badge badge-light">Forks: {repo.forks_count}</li>
-                            </ul>
-                        </div>
+            {repos.map(repo => (
+                <div key={repo._id} className="repo my-1 bg-white p-1">
+                    <div>
+                        <h4>
+                            <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>{repo.name}</a>
+                        </h4>
+                        <p>{repo.description}</p>
                     </div>
-                ))
-            )}
+                    <div>
+                        <ul>
+                            <li className="badge badge-primary">Stars: {repo.stargazers_count}</li>
+                            <li className="badge badge-dark">Watchers: {repo.watchers_count}</li>
+                            <li className="badge badge-light">Forks: {repo.forks_count}</li>
+                        </ul>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
